b682f1caa7155fab2028114d9533aa741680850e
Smarsden007 <seannmarsden@gmail.com>
2022-05-09 19:19:07 -0700

    Changes made to server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,46 @@
-// BUILD YOUR SERVER HERE
+const express = require('express')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const User = require('./users/model')
+
+const server = express()
+
+
+
+server.get('/api/users', (req, res) =>{
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+        .catch(err => {
+            res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack,
+        });
+    });
+});
+
+
+
+server.get('/api/users', (req, res) =>{
+    User.findById(req.params.id)
+    .then(users => {
+        console.log(user)
+    })
+        .catch(err => {
+            res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack,
+        });
+    });
+});
+
+server.use('*', (req,res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server;
+
